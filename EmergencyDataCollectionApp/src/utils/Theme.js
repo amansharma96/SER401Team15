3f64d8853e5eb27007e10c2bd1c34b6e7fb339bc
Sheldon Pang <SheldonLinkedin@yahoo.com>
2023-10-27 19:44:19 -0700

    lint fix

--- EmergencyDataCollectionApp/src/utils/Theme.js
+++ EmergencyDataCollectionApp/src/utils/Theme.js
@@ -1,17 +1,17 @@
 export default {
   COLORS: {
-    TEXT_BLACK: '#1C1C1E',
-    TEXT_GREY: '#7D7D7D',
-    BACKGROUND_YELLOW: '#FFCC00',
-    ERROR: '#F44336',
-    SUCCESS: '#4CAF50',
-    WARNING: '#FF9800',
-    BORDER_COLOR: '#D8D8DD',
+    TEXT_BLACK: "#1C1C1E",
+    TEXT_GREY: "#7D7D7D",
+    BACKGROUND_YELLOW: "#FFCC00",
+    ERROR: "#F44336",
+    SUCCESS: "#4CAF50",
+    WARNING: "#FF9800",
+    BORDER_COLOR: "#D8D8DD",
   },
   TYPOGRAPHY: {
     FONT_FAMILY: {
-      DEFAULT: 'Roboto',
-      HEADING: 'Roboto-Bold',
+      DEFAULT: "Roboto",
+      HEADING: "Roboto-Bold",
     },
     FONT_SIZE: {
       SMALL: 12,
@@ -24,9 +24,9 @@ export default {
       LARGE: 30,
     },
     FONT_WEIGHT: {
-      LIGHT: '300',
-      REGULAR: '400',
-      BOLD: '700',
+      LIGHT: "300",
+      REGULAR: "400",
+      BOLD: "700",
     },
   },
   SPACING: {
@@ -40,7 +40,7 @@ export default {
     BUTTON: 8,
   },
   SHADOW: {
-    DEFAULT: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    DEFAULT: "0px 2px 4px rgba(0, 0, 0, 0.1)",
   },
   // ... Feel free to add more shared styles here
 };

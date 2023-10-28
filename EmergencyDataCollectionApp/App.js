f09c66f3651880c90be90bca67b8d7fe5a17aa65
Sheldon Pang <SheldonLinkedin@yahoo.com>
2023-10-28 13:40:07 -0700

    draft

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+import Welcome from "./src/screens/Welcome";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Render the WelcomeScreen component */}
+      <Welcome />
+
+      {/* Comment out or remove this to hide the old content */}
+      {/*
       <Text>Hello SER401!</Text>
       <StatusBar style="auto" />
+      */}
     </View>
   );
 }
---
+++ EmergencyDataCollectionApp/src/utils/Images.js
@@ -0,0 +1,5 @@
+const Welcome = require("../../assets/images/logo.png");
+
+export default {
+  Welcome,
+};

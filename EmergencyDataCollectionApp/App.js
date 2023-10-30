34f800844d47d2456b91f49490ab022bdd53d88f
codyhughes96 <127884557+clhughe9@users.noreply.github.com>
2023-10-30 09:26:05 -0400

    updates to US44_TASK45

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import NavigationBar from './components/navigation/NavigationBar';
-import { NavigationContainer } from '@react-navigation/native';
+import NavigationBar from "./components/navigation/NavigationBar";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationBar></NavigationBar>
+      <NavigationBar />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop:StatusBar.currentHeight,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- EmergencyDataCollectionApp/components/navigation/NavigationBar.js
+++ EmergencyDataCollectionApp/components/navigation/NavigationBar.js
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import { Text, View, Button } from 'react-native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import * as React from "react";
+import { Text, View, Button } from "react-native";
+
+const Tab = createMaterialTopTabNavigator();
 
 function Page1({navigation}) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", }}>
       <Text>Page1!</Text>
       <Button
         title="Go to Page1"
-        onPress={() => navigation.navigate('App')}// Change this to the correct page to navigate to
+        onPress={() => navigation.navigate("App")}// Change this to the correct page to navigate to
       />
     </View>
   );
@@ -16,11 +18,11 @@ function Page1({navigation}) {
 
 function Page2({navigation}) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Page2!</Text>
       <Button
            title="Go to Page1"
-           onPress={() => navigation.navigate('App')}// Change this to the correct page to navigate to
+           onPress={() => navigation.navigate("App")}// Change this to the correct page to navigate to
            />
     </View>
   );
@@ -28,24 +30,22 @@ function Page2({navigation}) {
 
 function Page3({navigation}) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Page3!</Text>
         <Button
             title="Go to Page1"
-            onPress={() => navigation.navigate('App')} // Change this to the correct page to navigate to
+            onPress={() => navigation.navigate("App")} // Change this to the correct page to navigate to
             />
       </View>
     );
   }
-
-const Tab = createMaterialTopTabNavigator();
 
 function NavigationBar() {
     return (
         <Tab.Navigator
             screenOptions={{
                 tabBarLabelStyle: { fontSize: 12 },
-                tabBarStyle: { backgroundColor: '#ffcc00' },
+                tabBarStyle: { backgroundColor: "#ffcc00" },
             }}>
             <Tab.Screen name="Page1" component={Page1} />
             <Tab.Screen name="Page2" component={Page2} />

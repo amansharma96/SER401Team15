36cb9048442d828aaee26f4f0e8491f613a9645f
David Hanley <dchanley@asu.edu>
2023-10-30 17:45:55 -0700

    Made progress with First page of MYN report

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import StartMYNReport from "./src/screens/MYN/StartMYNReport";
 
 export default function App() {
   return (
+    /*
     <View style={styles.container}>
       <Text>Hello SER401!</Text>
       <StatusBar style="auto" />
+    </View>
+    */
+    <View>
+      <StartMYNReport />
     </View>
   );
 }

4089e94399d4d43bb8460c7cdb52f9e5309e3f54
Sheldon Pang <SheldonLinkedin@yahoo.com>
2023-10-30 21:24:36 -0700

    Reset App.js, added test for welcome screen

--- EmergencyDataCollectionApp/src/screens/welcome/Welcome.js
+++ EmergencyDataCollectionApp/src/screens/welcome/Welcome.js
@@ -8,14 +8,18 @@ const Welcome = () => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={Images.Welcome} style={styles.image} />
+        <Image
+          source={Images.Welcome}
+          style={styles.image}
+          testID="welcomeImage"
+        />
       </View>
       <View style={styles.textContainer}>
         <Text style={styles.title}>Emergency Ready</Text>
         <Text style={styles.subtitle}>Hazard Reporting</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} testID="getStartedButton">
           <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity>
       </View>

cc9766d0cd7f9dd8ea250afd003216bb62fab059
dchanley <dhanley@veritasglobal.com>
2023-11-01 14:54:59 -0700

    Made some minor changes found by lint

--- EmergencyDataCollectionApp/src/screens/MYN/MYNReportAnimals.js
+++ EmergencyDataCollectionApp/src/screens/MYN/MYNReportAnimals.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const MYNReportAnimals = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Hello World!</Text>
-        </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    });
-    
-    export default MYNReportAnimals
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MYNReportAnimals;
--- EmergencyDataCollectionApp/src/screens/MYN/MYNReprotEnd.js
+++ EmergencyDataCollectionApp/src/screens/MYN/MYNReprotEnd.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const MYNReprotEnd = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Hello World!</Text>
-        </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    });
-    
-    export default MYNReprotEnd
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MYNReprotEnd;

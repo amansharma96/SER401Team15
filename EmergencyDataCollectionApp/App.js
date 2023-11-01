69b64edeceba7fc795cce60b49edf7c038e0253c
dchanley <dhanley@veritasglobal.com>
2023-11-01 14:48:02 -0700

    Made fixes found by lint

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,13 +1,11 @@
 import { View } from "react-native";
-import NavigationBar from "./components/navigation/NavigationBar";
-import { NavigationContainer } from "@react-navigation/native";
 
 import MYNReportStart from "./src/screens/MYN/MYNReportStart";
 
 export default function App() {
   return (
     <View>
-      <MYNReportStart/>
+      <MYNReportStart />
     </View>
   );
 }

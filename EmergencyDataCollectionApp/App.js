4f07e1f22696ebf1b43be5c79984ef2e92c26d86
Sarah Hunt <shunt16@asu.edu>
2023-11-02 19:03:21 -0400

    reset app.js and moved HomeButton component

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -3,8 +3,18 @@ import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NavigationBar />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Hello SER401!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

68bdc4c499c69f5e4906185655bec840d18a96c1
codyhughes96 <127884557+clhughe9@users.noreply.github.com>
2023-11-04 16:27:09 -0400

    Update App.js
    Returned "App.js" to original state

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,11 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 
-import CERTScreen from "./src/screens/CERTScreen";
-
 export default function App() {
   return (
     <NavigationContainer>
-      <CERTScreen />
     </NavigationContainer>
   );
 }

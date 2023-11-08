445a8cd8d2b016d032973feacd122d5d2b36b578
Sheldon Pang <SheldonLinkedin@yahoo.com>
2023-11-08 13:47:02 -0800

    Refactor to make LocationService as a conponent

---
+++ EmergencyDataCollectionApp/src/utils/gps/App.js
@@ -0,0 +1,22 @@
+// This is an example on how to use the LocationService component
+// Returns a LocationService component that will return the user's location
+// Example of returned data: Location Data: {"coords": {"accuracy": 100, "altitude": 88.9000015258789, "altitudeAccuracy": 100, "heading": 0, "latitude": *, "longitude": *, "speed": 0}, "mocked": false, "timestamp": 1700200488002}
+// TODO - remove this file
+
+import React from "react";
+
+import LocationService from "./src/utils/gps/locationService";
+
+const App = () => {
+  const handleLocation = (data) => {
+    if (data.error) {
+      console.error("Location Error:", data.error);
+    } else {
+      console.log("Location Data:", data);
+    }
+  };
+
+  return <LocationService onLocationObtained={handleLocation} />;
+};
+
+export default App;

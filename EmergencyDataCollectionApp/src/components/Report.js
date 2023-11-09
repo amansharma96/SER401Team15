d6fafb30d45d31dd116652f51b841a0aba5ad5a6
Sarah Hunt <shunt16@asu.edu>
2023-11-09 13:15:29 -0500

    Added checkbox to reports screen

--- EmergencyDataCollectionApp/src/components/Report.js
+++ EmergencyDataCollectionApp/src/components/Report.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text} from 'react-native';
+import Checkbox from 'expo-checkbox';
+import Theme from '../utils/Theme';
 
-export default function HomeButton(props) {
+export default function Report(props) {
     const { address = '123 Sesame Street Tempe, AZ 30204', title = 'Fire Incident' } = props;
+    const [isChecked, setChecked] = useState(false);
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.address}>{address}</Text>
+            <Checkbox
+                style={styles.checkbox}
+                value={isChecked}
+                onValueChange={setChecked}
+                color={isChecked ? '#4630EB' : undefined}
+            />
         </View>
     );
 }
@@ -16,27 +25,25 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
         justifyContent: 'center',
         width: '80%',
-        paddingVertical: 14,
-        paddingHorizontal: 14,
-        borderRadius: 12,
+        paddingVertical: Theme.BUTTON_PADDING.VERTICAL,
+        paddingHorizontal: Theme.BUTTON_PADDING.HORIZONTAL,
+        borderRadius: Theme.RADIUS.BUTTON,
         elevation: 3,
-        backgroundColor: '#ffcc00',
+        backgroundColor: Theme.COLORS.BACKGROUND_YELLOW,
     },
     title: {
-        color: '#1c1c1e',
-        fontSize: 18,
-        fontFamily: 'Arial',
-        lineHeight: 24,
-        fontWeight: 'bold',
-        letterSpacing: 0.1
+        color: Theme.COLORS.TEXT_BLACK,
+        fontSize: Theme.TYPOGRAPHY.FONT_SIZE.MEDIUM,
+        fontFamily: Theme.TYPOGRAPHY.FONT_FAMILY.DEFAULT,
+        lineHeight: Theme.TYPOGRAPHY.LINE_HEIGHT.MEDIUM,
+        fontWeight: Theme.TYPOGRAPHY.FONT_WEIGHT.BOLD,
     },
     address: {
-        color: '#1c1c1e',
-        fontSize: 14,
-        fontFamily: 'Arial',
-        lineHeight: 24,
-        fontWeight: 'normal',
-        letterSpacing: 0.1,
+        color: Theme.COLORS.TEXT_BLACK,
+        fontSize: Theme.TYPOGRAPHY.FONT_SIZE.SMALL,
+        fontFamily: Theme.TYPOGRAPHY.FONT_FAMILY.DEFAULT,
+        lineHeight: Theme.TYPOGRAPHY.LINE_HEIGHT.SMALL,
+        fontWeight: Theme.TYPOGRAPHY.FONT_WEIGHT.REGULAR,
         marginLeft: 20
     },
 })

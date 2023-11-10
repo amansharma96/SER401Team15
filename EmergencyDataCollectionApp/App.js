e9f6aed8a89ec73c9a9333f9fffc2193c33e94ee
famandee <famandee@asu.edu>
2023-11-10 15:19:55 -0800

    Rebase develop

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,24 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import NavigationBar from './src/navigation/NavigationBar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+
+import MYNReportNavigation from "./src/navigation/MYNNavigation/MYNReportNavigation";
+import MainScreen from "./src/screens/MainScreen";
+import Welcome from "./src/screens/welcome/Welcome";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
-    <StatusBar style="auto"/>
-      
-      <NavigationBar />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen
+          name="StartNewMYNReport"
+          component={MYNReportNavigation}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

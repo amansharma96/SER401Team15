28bf54010df25c96afdc9b83839ae0dcded378b4
codyhughes96 <127884557+clhughe9@users.noreply.github.com>
2023-11-10 17:27:26 -0500

    Lint error fixes

--- EmergencyDataCollectionApp/src/screens/cert/CERTScreen.js
+++ EmergencyDataCollectionApp/src/screens/cert/CERTScreen.js
@@ -1,47 +1,49 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import * as React from "react";
 import { View, Button, StyleSheet } from "react-native";
+
+import ExtraPage from "./ExtraPage";
+import HazardsPage from "./HazardsPage";
 import InfoPage from "./InfoPage";
 import LocationPage from "./LocationPage";
-import HazardsPage from "./HazardsPage";
 import PeoplePage from "./PeoplePage";
-import ExtraPage from "./ExtraPage";
 
 const Tab = createMaterialTopTabNavigator();
 
-const page1Complete = false;
-const page2Complete = false;
-const page3Complete = false;
-const page4Complete = false;
-const page5Complete = true; //No required inputs on this page
+// TODO: implement conditional save button
+//const page1Complete = false;
+//const page2Complete = false;
+//const page3Complete = false;
+//const page4Complete = false;
+//const page5Complete = true; //No required inputs on this page
 
 function CERTScreen() {
   return (
     <View style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
-          <Tab.Navigator
-              screenOptions={{
-                tabBarActiveTintColor: "#111111",
-                tabBarLabelStyle: { fontSize: 11, textAlignVertical: "bottom" },
-                tabBarStyle: { backgroundColor: "#ffcc00", height: "8%" },
-              }}
-          >
-            <Tab.Group name="CERT Report Page" />
-            <Tab.Screen name="Info" component={InfoPage} />
-            <Tab.Screen name="Location" component={LocationPage} />
-            <Tab.Screen name="Hazards" component={HazardsPage} />
-            <Tab.Screen name="People" component={PeoplePage} />
-            <Tab.Screen name="Extra Info" component={ExtraPage} />
-          </Tab.Navigator>
-        </View>
-        <View style={styles.SAVEBUTTON}>
-          <Button
-            title="Save Report"
-            // disabled={ !page1Complete || !page2Complete || !page3Complete || !page4Complete || !page5Complete }
-            onPress={null} // Change this to saving the report
-          />
-        </View>
+      <View style={{ flex: 1 }}>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: "#111111",
+            tabBarLabelStyle: { fontSize: 11, textAlignVertical: "bottom" },
+            tabBarStyle: { backgroundColor: "#ffcc00", height: "8%" },
+          }}
+        >
+          <Tab.Group name="CERT Report Page" />
+          <Tab.Screen name="Info" component={InfoPage} />
+          <Tab.Screen name="Location" component={LocationPage} />
+          <Tab.Screen name="Hazards" component={HazardsPage} />
+          <Tab.Screen name="People" component={PeoplePage} />
+          <Tab.Screen name="Extra Info" component={ExtraPage} />
+        </Tab.Navigator>
       </View>
+      <View style={styles.SAVEBUTTON}>
+        <Button
+          title="Save Report"
+          // disabled={ !page1Complete || !page2Complete || !page3Complete || !page4Complete || !page5Complete }
+          onPress={null} // Change this to saving the report
+        />
+      </View>
+    </View>
   );
 }
 
--- EmergencyDataCollectionApp/src/screens/cert/HazardsPage.js
+++ EmergencyDataCollectionApp/src/screens/cert/HazardsPage.js
@@ -1,111 +1,131 @@
 import * as React from "react";
-import { Text, View, Button, StyleSheet, TextInput } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 
 function HazardsPage() {
+  const [setStructureType] = React.useState("");
 
-    const structureTypes = [
-      { key: '1', value: 'Structure 1' },
-      { key: '2', value: 'Structure 2' },
-      { key: '3', value: 'Structure 3' },
-      { key: '4', value: 'Structure 4' },
-      { key: '5', value: 'Structure 5' },
-      { key: '6', value: 'Structure 6' },
-      { key: '7', value: 'Structure 7' },
-    ]
-  
-    return (
+  const structureTypes = [
+    { key: "1", value: "Structure 1" },
+    { key: "2", value: "Structure 2" },
+    { key: "3", value: "Structure 3" },
+    { key: "4", value: "Structure 4" },
+    { key: "5", value: "Structure 5" },
+    { key: "6", value: "Structure 6" },
+    { key: "7", value: "Structure 7" },
+  ];
+
+  return (
+    <View style={styles.CONTAINER}>
       <View style={styles.CONTAINER}>
-        <View style={styles.CONTAINER}>
-          <Text style={styles.HEADER1TEXT}>Hazard Information</Text>
-          <View style={styles.CONTAINER_ROW}>
-            <Text style={styles.TEXT}>*Status of FIRE hazards:</Text>
-          </View>
-          <View style={styles.CONTAINER_ROW_DROPDOWN}>
-              <SelectList setSelected={(val) => setStructureType(val)} data={structureTypes} save="value" />
-          </View>
-          <View style={styles.CONTAINER_ROW}>
-            <Text style={styles.TEXT}>*Status of PROPANE or GAS hazards:</Text>
-          </View>
-          <View style={styles.CONTAINER_ROW_DROPDOWN}>
-              <SelectList setSelected={(val) => setStructureType(val)} data={structureTypes} save="value" />
-          </View>
-          <View style={styles.CONTAINER_ROW}>
-            <Text style={styles.TEXT}>*Status of WATER hazards: </Text>
-          </View>
-          <View style={styles.CONTAINER_ROW_DROPDOWN}>
-              <SelectList setSelected={(val) => setStructureType(val)} data={structureTypes} save="value" />
-          </View>
-          <View style={styles.CONTAINER_ROW}>
-            <Text style={styles.TEXT}>*Status of ELECTRICAL hazards:</Text>
-          </View>
-          <View style={styles.CONTAINER_ROW_DROPDOWN}>
-              <SelectList setSelected={(val) => setStructureType(val)} data={structureTypes} save="value" />
-          </View>
-          <View style={styles.CONTAINER_ROW}>
-            <Text style={styles.TEXT}>*Status of CHEMICAL hazards: </Text>
-          </View>
-          <View style={styles.CONTAINER_ROW_DROPDOWN}>
-              <SelectList setSelected={(val) => setStructureType(val)} data={structureTypes} save="value" />
-          </View>
+        <Text style={styles.HEADER1TEXT}>Hazard Information</Text>
+        <View style={styles.CONTAINER_ROW}>
+          <Text style={styles.TEXT}>*Status of FIRE hazards:</Text>
+        </View>
+        <View style={styles.CONTAINER_ROW_DROPDOWN}>
+          <SelectList
+            setSelected={(val) => setStructureType(val)}
+            data={structureTypes}
+            save="value"
+          />
+        </View>
+        <View style={styles.CONTAINER_ROW}>
+          <Text style={styles.TEXT}>*Status of PROPANE or GAS hazards:</Text>
+        </View>
+        <View style={styles.CONTAINER_ROW_DROPDOWN}>
+          <SelectList
+            setSelected={(val) => setStructureType(val)}
+            data={structureTypes}
+            save="value"
+          />
+        </View>
+        <View style={styles.CONTAINER_ROW}>
+          <Text style={styles.TEXT}>*Status of WATER hazards: </Text>
+        </View>
+        <View style={styles.CONTAINER_ROW_DROPDOWN}>
+          <SelectList
+            setSelected={(val) => setStructureType(val)}
+            data={structureTypes}
+            save="value"
+          />
+        </View>
+        <View style={styles.CONTAINER_ROW}>
+          <Text style={styles.TEXT}>*Status of ELECTRICAL hazards:</Text>
+        </View>
+        <View style={styles.CONTAINER_ROW_DROPDOWN}>
+          <SelectList
+            setSelected={(val) => setStructureType(val)}
+            data={structureTypes}
+            save="value"
+          />
+        </View>
+        <View style={styles.CONTAINER_ROW}>
+          <Text style={styles.TEXT}>*Status of CHEMICAL hazards: </Text>
+        </View>
+        <View style={styles.CONTAINER_ROW_DROPDOWN}>
+          <SelectList
+            setSelected={(val) => setStructureType(val)}
+            data={structureTypes}
+            save="value"
+          />
         </View>
       </View>
-    );
+    </View>
+  );
 }
 export default HazardsPage;
 
-
 const styles = StyleSheet.create({
-    CONTAINER: {
-      flexDirection: "column",
-      alignItems: "bottom",
-      justifyContent: "bottom",
-      width: "100%",
-    },
-    CONTAINER_ROW: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "left",
-      width: "100%",
-    },
-    CONTAINER_ROW_TEMP: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-    },
-    CONTAINER_ROW_DROPDOWN: {
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-    },
-    BUTTONCONTAINER: {
-      flexDirection: "row",
-      marginTop: 10,
-      justifyContent: "center",
-      width: "75%",
-    },
-    HEADER1TEXT: {
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-    HEADER2TEXT: {
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    TEXT: {
-      fontSize: 15,
-    },
-    TEXT_TEMP: {
-      fontSize: 15,
-      color: "red",
-    },
-    SAVEBUTTON: {
-      flexDirection: "column",
-      verticalAlign: "bottom",
-      alignSelf: "center",
-      justifyContent: "center",
-      width: "75%",
-      marginVertical: 20,
-    },
-  });
+  CONTAINER: {
+    flexDirection: "column",
+    alignItems: "bottom",
+    justifyContent: "bottom",
+    width: "100%",
+  },
+  CONTAINER_ROW: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "left",
+    width: "100%",
+  },
+  CONTAINER_ROW_TEMP: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  CONTAINER_ROW_DROPDOWN: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  BUTTONCONTAINER: {
+    flexDirection: "row",
+    marginTop: 10,
+    justifyContent: "center",
+    width: "75%",
+  },
+  HEADER1TEXT: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  HEADER2TEXT: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  TEXT: {
+    fontSize: 15,
+  },
+  TEXT_TEMP: {
+    fontSize: 15,
+    color: "red",
+  },
+  SAVEBUTTON: {
+    flexDirection: "column",
+    verticalAlign: "bottom",
+    alignSelf: "center",
+    justifyContent: "center",
+    width: "75%",
+    marginVertical: 20,
+  },
+});

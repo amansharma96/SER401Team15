dcd19ad1499db43adf4eaf317715ff033d8e7c4b
Sarah Hunt <shunt16@asu.edu>
2023-11-17 18:34:10 -0500

    Added tests for all button navigation from main

--- EmergencyDataCollectionApp/__tests__/Navigation/MainButtons_test.js
+++ EmergencyDataCollectionApp/__tests__/Navigation/MainButtons_test.js
@@ -11,3 +11,79 @@ it("Should navigate to MYN reporting page", () => {
 
   expect(getByTestId("MYNstart")).toBeTruthy();
 });
+
+it("Should navigate to saved MYN reports page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const savedMYNreportButton = getByText("Review saved MYN Reports");
+  fireEvent.press(savedMYNreportButton);
+
+  expect(getByTestId("MYNsaved")).toBeTruthy(); */
+});
+
+it("Should navigate to CERT reporting page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const newCERTreportButton = getByText("Start a new CERT Report");
+  fireEvent.press(newCERTreportButton);
+
+  expect(getByTestId("CERTstart")).toBeTruthy();
+  */
+});
+
+it("Should navigate to saved CERT reports page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const savedCERTreportButton = getByText("Review saved CERT Reports");
+  fireEvent.press(savedCERTreportButton);
+
+  expect(getByTestId("CERTsaved")).toBeTruthy(); */
+});
+
+it("Should navigate to HAZARD reporting page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const newHAZARDreportButton = getByText("Start a new HAZARD Report");
+  fireEvent.press(newHAZARDreportButton);
+
+  expect(getByTestId("HAZARDstart")).toBeTruthy();
+  */
+});
+
+it("Should navigate to saved HAZARD reports page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const savedHAZARDreportButton = getByText("Review saved HAZARD Reports");
+  fireEvent.press(savedHAZARDreportButton);
+
+  expect(getByTestId("HAZARDsaved")).toBeTruthy(); */
+});
+
+it("Should navigate to export reports page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const exportReportsButton = getByText("Copy saved files to USB storage");
+  fireEvent.press(exportReportsButton);
+
+  expect(getByTestId("Export")).toBeTruthy(); */
+});
+
+it("Should navigate to instructions page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const instructionsButton = getByText("Instructions");
+  fireEvent.press(instructionsButton);
+
+  expect(getByTestId("instructions")).toBeTruthy(); */
+});
+
+it("Should navigate to settings page", () => {
+  /* const { getByText, getByTestId } = render(<MainNavigation />);
+
+  const settingsButton = getByText("Settings");
+  fireEvent.press(settingsButton);
+
+  expect(getByTestId("settings")).toBeTruthy(); */
+});
+
+

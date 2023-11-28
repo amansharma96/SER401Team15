58032020c984d535edc4a8bf2fdf97bb9c2faaa3
Sheldon Pang <SheldonLinkedin@yahoo.com>
2023-11-28 09:23:36 -0800

    refactor location manager and added function to refine the GPS data to a more accurate reading by averaging

--- EmergencyDataCollectionApp/src/components/LocationManager/App.js
+++ EmergencyDataCollectionApp/src/components/LocationManager/App.js
@@ -1,40 +1,17 @@
 //Example of how to use the LocationManager_v2 component
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View, Text, Button } from "react-native";
 
 import LocationManager_v2 from "./src/components/LocationManager/LocationManager_v2";
-import { GPS_FETCHING_TIMEOUT } from "./src/utils/constants/GlobalConstants";
 import LocationService_v2 from "./src/utils/gps/locationService_v2";
 
 const App = () => {
-  const [isLoggingLocationData, setIsLoggingLocationData] = useState(false);
-
   const {
     locationData,
     isFetchingLocation,
-    startFetchingLocation,
-    stopFetchingLocation,
+    onStartFetch,
     handleLocationUpdate,
   } = LocationManager_v2();
-
-  const onStartFetch = () => {
-    startFetchingLocation();
-
-    setTimeout(() => {
-      stopFetchingLocation();
-      setIsLoggingLocationData(true);
-    }, GPS_FETCHING_TIMEOUT);
-  };
-
-  useEffect(() => {
-    if (isLoggingLocationData) {
-      console.log("Location Data:");
-      locationData.forEach((location, index) => {
-        console.log(`Location ${index}:`, location);
-      });
-      setIsLoggingLocationData(false);
-    }
-  }, [isLoggingLocationData, locationData]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
--- EmergencyDataCollectionApp/src/components/LocationManager/LocationManager_v2.js
+++ EmergencyDataCollectionApp/src/components/LocationManager/LocationManager_v2.js
@@ -1,13 +1,20 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { Alert } from "react-native";
+
+import { GPS_FETCHING_TIMEOUT } from "../../utils/constants/GlobalConstants";
 
 const LocationManager_v2 = () => {
   const [locationData, setLocationData] = useState([]);
   const [isFetchingLocation, setIsFetchingLocation] = useState(false);
+  const [isLoggingLocationData, setIsLoggingLocationData] = useState(false);
 
   const handleLocationUpdate = useCallback((location) => {
-    if (location?.coords) {
+    if (location?.coords && location.coords.accuracy <= 10) {
       setLocationData((prevData) => [...prevData, location]);
+    } else if (location?.coords) {
+      console.log(
+        `Ignored location with accuracy: ${location.coords.accuracy}`,
+      );
     } else {
       Alert.alert(
         "Location Error: ",
@@ -25,12 +32,63 @@ const LocationManager_v2 = () => {
     setIsFetchingLocation(false);
   }, []);
 
+  const calculateAverageLocationAndAccuracy = (locations) => {
+    const validLocations = locations.filter((loc) => loc.coords.accuracy <= 10);
+
+    const averageData = validLocations.reduce(
+      (acc, loc) => {
+        acc.latitude += loc.coords.latitude;
+        acc.longitude += loc.coords.longitude;
+        acc.accuracy += loc.coords.accuracy;
+        return acc;
+      },
+      { latitude: 0, longitude: 0, accuracy: 0 },
+    );
+
+    if (validLocations.length > 0) {
+      averageData.latitude /= validLocations.length;
+      averageData.longitude /= validLocations.length;
+      averageData.accuracy /= validLocations.length;
+    }
+
+    return averageData;
+  };
+
+  const onStartFetch = useCallback(() => {
+    startFetchingLocation();
+
+    setTimeout(() => {
+      stopFetchingLocation();
+      setIsLoggingLocationData(true);
+    }, GPS_FETCHING_TIMEOUT);
+  }, [startFetchingLocation, stopFetchingLocation]);
+
+  useEffect(() => {
+    if (isLoggingLocationData) {
+      console.log("Location Data:");
+      locationData.forEach((location, index) => {
+        console.log(`Location ${index}:`, location);
+      });
+
+      const averageData = calculateAverageLocationAndAccuracy(locationData);
+      console.log(
+        "Average Location:",
+        averageData.latitude,
+        averageData.longitude,
+      );
+      console.log("Average Accuracy:", averageData.accuracy);
+
+      setIsLoggingLocationData(false);
+    }
+  }, [isLoggingLocationData, locationData]);
+
   return {
     locationData,
     isFetchingLocation,
     startFetchingLocation,
     stopFetchingLocation,
     handleLocationUpdate,
+    onStartFetch,
   };
 };
 

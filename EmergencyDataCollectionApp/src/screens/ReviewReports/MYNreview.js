85385e51493ad4a5ad7e9f9a836f882f4d2e969a
Sarah Hunt <shunt16@asu.edu>
2023-11-29 18:28:11 -0500

    Created screen files for reviewing reportss

---
+++ EmergencyDataCollectionApp/src/screens/ReviewReports/MYNreview.js
@@ -0,0 +1,13 @@
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Checkbox } from "native-base";
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+import mockReportsData from "../../utils/constants/mockReportsData";
+import Theme from "../../utils/Theme";

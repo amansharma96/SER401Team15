ca5fcbc0a20f6bd7eee3ca00e986b32ffd4cfed8
Sheldon Pang <SheldonLinkedin@yahoo.com>
2024-01-16 19:24:09 -0800

    Add custom spinner component

---
+++ EmergencyDataCollectionApp/__tests__/src/components/Spinner_test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen, waitFor } from "@testing-library/react-native";
+import CustomSpinner from "../../../src/components/Spinner/CustomSpinner.js";
+
+describe('CustomSpinner', () => {
+    it('should display the spinner and custom text, then disappear after the specified duration', async () => {
+        const customText = "Loading...";
+        const duration = 500;
+
+        render(<CustomSpinner text={customText} duration={duration} />);
+
+        // Check if spinner and text are in the document initially
+        expect(screen.getByText(customText)).toBeInTheDocument();
+        expect(screen.getByRole('progressbar')).toBeInTheDocument();
+
+        // Wait for the specified duration and check if spinner is removed
+        await waitFor(() => {
+            expect(screen.queryByText(customText)).not.toBeInTheDocument();
+        }, { timeout: duration + 100 });
+    });
+});
---
+++ EmergencyDataCollectionApp/src/components/Spinner/CustomSpinner.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { Spinner, HStack, Text } from "@gluestack-ui/themed";
+
+const CustomSpinner = ({ text, duration }) => {
+    const [showSpinner, setShowSpinner] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setShowSpinner(false), duration);
+        return () => clearTimeout(timer);
+    }, [duration]);
+
+    if (!showSpinner) return null;
+
+    return (
+        <HStack space="sm">
+            <Spinner />
+            <Text size="md">{text}</Text>
+        </HStack>
+    );
+};
+
+export default CustomSpinner;

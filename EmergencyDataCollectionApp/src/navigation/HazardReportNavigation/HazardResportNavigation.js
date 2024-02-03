a6b26a0c582a5837008091aab2c0e2edd9bcae77
famandee <92889502+amansharma96@users.noreply.github.com>
2024-02-03 08:43:28 -0800

    Update HazardResportNavigation.js

--- EmergencyDataCollectionApp/src/navigation/HazardReportNavigation/HazardResportNavigation.js
+++ EmergencyDataCollectionApp/src/navigation/HazardReportNavigation/HazardResportNavigation.js
@@ -1,25 +1,26 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { HazardReportProvider } from "../../screens/HazardReports/HazardReportsContext";
 import * as React from "react";
+
 import FirstScreen from "../../screens/HazardReports/FirstScreen";
 import SecondScreen from "../../screens/HazardReports/SecondScreen";
 import ThirdScreen from "../../screens/HazardReports/ThirdScreen";
+import { HazardReportProvider } from "../../screens/HazardReports/HazardReportsContext";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function NavigationBar() {
   return (
     <HazardReportProvider>
-    <Tab.Navigator
-    screenOptions={{
-      tabBarLabelStyle: { fontSize: 11, fontWeight: "bold" },
-      tabBarStyle: { backgroundColor: "#ffcc00", marginTop: 20 },
-    }}
-    >
-      <Tab.Screen name="Report" component={FirstScreen} />
-      <Tab.Screen name="Notes" component={SecondScreen} />
-      <Tab.Screen name="Finalise" component={ThirdScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelStyle: { fontSize: 11, fontWeight: "bold" },
+          tabBarStyle: { backgroundColor: "#ffcc00", marginTop: 20 },
+        }}
+      >
+        <Tab.Screen name="Report" component={FirstScreen} />
+        <Tab.Screen name="Notes" component={SecondScreen} />
+        <Tab.Screen name="Finalise" component={ThirdScreen} />
+      </Tab.Navigator>
     </HazardReportProvider>
   );
 }

c1c0dfdddf7eab68bc6c4948723492cbdeefd144
Sheldon Pang <SheldonLinkedin@yahoo.com>
2024-02-17 20:49:35 -0800

    Lint fix

--- EmergencyDataCollectionApp/__tests__/src/components/CustomSelect/CustomSelect_test.js
+++ EmergencyDataCollectionApp/__tests__/src/components/CustomSelect/CustomSelect_test.js
@@ -1,73 +1,82 @@
-import React from 'react';
-import {render, fireEvent} from '@testing-library/react-native';
+import { render, fireEvent } from "@testing-library/react-native";
+import React from "react";
 
 import CustomSelect from "../../../../src/components/CustomSelect/CustomSelect";
 import NativeBaseTestUtils from "../../../../src/utils/NativeBaseTestUtils/NativeBaseTestUtils";
 
-describe('CustomSelect Component', () => {
-    const mockOnChange = jest.fn();
-    const items = [
-        { label: 'Item 1', value: '1' },
-        { label: 'Item 2', value: '2' },
-        { label: 'Item 3', value: '3' },
-    ];
+describe("CustomSelect Component", () => {
+  const mockOnChange = jest.fn();
+  const items = [
+    { label: "Item 1", value: "1" },
+    { label: "Item 2", value: "2" },
+    { label: "Item 3", value: "3" },
+  ];
 
-    it('renders correctly with mandatory props', () => {
-        const { getAllByLabelText } = render(
-            <NativeBaseTestUtils>
-                <CustomSelect items={items} onChange={mockOnChange} />
-            </NativeBaseTestUtils>
-        );
+  it("renders correctly with mandatory props", () => {
+    const { getAllByLabelText } = render(
+      <NativeBaseTestUtils>
+        <CustomSelect items={items} onChange={mockOnChange} />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getAllByLabelText('Select an option')).toBeTruthy();
-    });
+    expect(getAllByLabelText("Select an option")).toBeTruthy();
+  });
 
-    it('handles item selection', () => {
-        const { getByRole } = render(
-            <NativeBaseTestUtils>
-            <CustomSelect items={items} onChange={mockOnChange} />
-            </NativeBaseTestUtils>
-        );
+  it("handles item selection", () => {
+    const { getByRole } = render(
+      <NativeBaseTestUtils>
+        <CustomSelect items={items} onChange={mockOnChange} />
+      </NativeBaseTestUtils>,
+    );
 
-        fireEvent(getByRole('button'), 'onPress');
-        fireEvent(getByRole('text', { name: 'Item 2' }), 'press');
-        expect(mockOnChange).toHaveBeenCalledWith('2');
-    });
+    fireEvent(getByRole("button"), "onPress");
+    fireEvent(getByRole("text", { name: "Item 2" }), "press");
+    expect(mockOnChange).toHaveBeenCalledWith("2");
+  });
 
-    it('updates selected value when prop changes', () => {
-        const { getByText, rerender } = render(
-            <NativeBaseTestUtils>
-            <CustomSelect items={items} onChange={mockOnChange} selectedValue="1" />
-            </NativeBaseTestUtils>
-        );
+  it("updates selected value when prop changes", () => {
+    const { getByText, rerender } = render(
+      <NativeBaseTestUtils>
+        <CustomSelect items={items} onChange={mockOnChange} selectedValue="1" />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByText('Item 1')).toBeTruthy();
+    expect(getByText("Item 1")).toBeTruthy();
 
-        rerender(
-            <NativeBaseTestUtils>
-            <CustomSelect items={items} onChange={mockOnChange} selectedValue="2"/>
-            </NativeBaseTestUtils>
-                );
-        expect(getByText('Item 2')).toBeTruthy();
-    });
+    rerender(
+      <NativeBaseTestUtils>
+        <CustomSelect items={items} onChange={mockOnChange} selectedValue="2" />
+      </NativeBaseTestUtils>,
+    );
+    expect(getByText("Item 2")).toBeTruthy();
+  });
 
-    it('displays error message when isInvalid is true', () => {
-        const { getByText } = render(
-            <NativeBaseTestUtils>
-            <CustomSelect items={items} onChange={mockOnChange} isInvalid={true} errorMessage="Custom error message" />
-            </NativeBaseTestUtils>
-        );
+  it("displays error message when isInvalid is true", () => {
+    const { getByText } = render(
+      <NativeBaseTestUtils>
+        <CustomSelect
+          items={items}
+          onChange={mockOnChange}
+          isInvalid
+          errorMessage="Custom error message"
+        />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByText('Custom error message')).toBeTruthy();
-    });
+    expect(getByText("Custom error message")).toBeTruthy();
+  });
 
-    it('renders label when provided', () => {
-        const { getByText } = render(
-            <NativeBaseTestUtils>
-            <CustomSelect items={items} onChange={mockOnChange} label="Custom Label" />
-            </NativeBaseTestUtils>
-        );
+  it("renders label when provided", () => {
+    const { getByText } = render(
+      <NativeBaseTestUtils>
+        <CustomSelect
+          items={items}
+          onChange={mockOnChange}
+          label="Custom Label"
+        />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByText('Custom Label')).toBeTruthy();
-    });
+    expect(getByText("Custom Label")).toBeTruthy();
+  });
 });
--- EmergencyDataCollectionApp/__tests__/src/components/CustomTextArea/CustomTextArea_test.js
+++ EmergencyDataCollectionApp/__tests__/src/components/CustomTextArea/CustomTextArea_test.js
@@ -1,64 +1,67 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render, fireEvent } from "@testing-library/react-native";
+import React from "react";
 
 import CustomTextArea from "../../../../src/components/CustomTextArea/CustomTextArea";
 import NativeBaseTestUtils from "../../../../src/utils/NativeBaseTestUtils/NativeBaseTestUtils";
 
-describe('CustomTextArea Component', () => {
-    const mockOnChangeText = jest.fn();
+describe("CustomTextArea Component", () => {
+  const mockOnChangeText = jest.fn();
 
-    it('renders correctly with default props', () => {
-        const { getByPlaceholderText } = render(
-            <NativeBaseTestUtils>
-            <CustomTextArea onChangeText={mockOnChangeText} />
-            </NativeBaseTestUtils>
-        );
+  it("renders correctly with default props", () => {
+    const { getByPlaceholderText } = render(
+      <NativeBaseTestUtils>
+        <CustomTextArea onChangeText={mockOnChangeText} />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByPlaceholderText('Enter text')).toBeTruthy();
-    });
+    expect(getByPlaceholderText("Enter text")).toBeTruthy();
+  });
 
-    it('displays provided label', () => {
-        const { getByText } = render(
-            <NativeBaseTestUtils>
-            <CustomTextArea label="Test Label" onChangeText={mockOnChangeText} />
-            </NativeBaseTestUtils>
-        );
+  it("displays provided label", () => {
+    const { getByText } = render(
+      <NativeBaseTestUtils>
+        <CustomTextArea label="Test Label" onChangeText={mockOnChangeText} />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByText('Test Label')).toBeTruthy();
-    });
+    expect(getByText("Test Label")).toBeTruthy();
+  });
 
-    it('shows custom placeholder when provided', () => {
-        const { getByPlaceholderText } = render(
-            <NativeBaseTestUtils>
-            <CustomTextArea placeholder="Custom Placeholder" onChangeText={mockOnChangeText} />
-            </NativeBaseTestUtils>
-        );
+  it("shows custom placeholder when provided", () => {
+    const { getByPlaceholderText } = render(
+      <NativeBaseTestUtils>
+        <CustomTextArea
+          placeholder="Custom Placeholder"
+          onChangeText={mockOnChangeText}
+        />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByPlaceholderText('Custom Placeholder')).toBeTruthy();
-    });
+    expect(getByPlaceholderText("Custom Placeholder")).toBeTruthy();
+  });
 
-    it('calls onChangeText when text changes', () => {
-        const { getByPlaceholderText } = render(
-            <NativeBaseTestUtils>
-            <CustomTextArea onChangeText={mockOnChangeText} />
-            </NativeBaseTestUtils>
-        );
+  it("calls onChangeText when text changes", () => {
+    const { getByPlaceholderText } = render(
+      <NativeBaseTestUtils>
+        <CustomTextArea onChangeText={mockOnChangeText} />
+      </NativeBaseTestUtils>,
+    );
 
-        fireEvent.changeText(getByPlaceholderText('Enter text'), 'New text');
-        expect(mockOnChangeText).toHaveBeenCalledWith('New text');
-    });
+    fireEvent.changeText(getByPlaceholderText("Enter text"), "New text");
+    expect(mockOnChangeText).toHaveBeenCalledWith("New text");
+  });
 
-    it('renders error message when isInvalid is true', () => {
-        const { getByText } = render(
-            <NativeBaseTestUtils>
-            <CustomTextArea
-                isInvalid
-                errorMessage="Custom Error Message"
-                onChangeText={mockOnChangeText}
-            />
-            </NativeBaseTestUtils>
-        );
+  it("renders error message when isInvalid is true", () => {
+    const { getByText } = render(
+      <NativeBaseTestUtils>
+        <CustomTextArea
+          isInvalid
+          errorMessage="Custom Error Message"
+          onChangeText={mockOnChangeText}
+        />
+      </NativeBaseTestUtils>,
+    );
 
-        expect(getByText('Custom Error Message')).toBeTruthy();
-    });
+    expect(getByText("Custom Error Message")).toBeTruthy();
+  });
 });

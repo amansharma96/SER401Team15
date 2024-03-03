203f378672295aa6a6baf789c203160a5b6b3d6e
Sheldon Pang <SheldonLinkedin@yahoo.com>
2024-03-03 00:03:25 -0800

    clean up

--- EmergencyDataCollectionApp/src/screens/Welcome/Welcome.js
+++ EmergencyDataCollectionApp/src/screens/Welcome/Welcome.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
-import CustomInput from "../../components/CustomForms/GluestackUI/CustomInput/CustomInput";
 import { setupDatabase } from "../../utils/Database/OfflineSQLiteDB";
 import Images from "../../utils/Images";
 
@@ -10,13 +9,6 @@ const Welcome = ({ navigation }) => {
   useEffect(() => {
     setupDatabase(() => {});
   }, []);
-
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setInputValue(e.nativeEvent.text);
-    console.log("New input value: ", inputValue);
-  };
 
   return (
     <View style={styles.container}>
@@ -31,18 +23,6 @@ const Welcome = ({ navigation }) => {
         <Text style={styles.title}>Emergency Ready</Text>
         <Text style={styles.subtitle}>Hazard Reporting</Text>
       </View>
-      <CustomInput
-        label="Email Address"
-        placeholder="Enter your email"
-        isRequired
-        value={inputValue}
-        onChange={handleInputChange}
-        errorMessage="Please enter a valid email address"
-        helperMessage="We will never share your email."
-        displayHelperMessage
-        w="80%"
-        isInvalid={false}
-      />
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}

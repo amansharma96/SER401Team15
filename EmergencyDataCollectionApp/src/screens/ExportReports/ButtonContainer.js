4b2707bed0957d844c981d05c6e1c9104edd23e6
Sarah Hunt <shunt16@asu.edu>
2024-03-22 15:34:02 -0400

    Added alert on delete

--- EmergencyDataCollectionApp/src/screens/ExportReports/ButtonContainer.js
+++ EmergencyDataCollectionApp/src/screens/ExportReports/ButtonContainer.js
@@ -1,102 +1,92 @@
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
 import {
-    View,
-    Text,
-    TouchableOpacity,
-  } from "react-native";
-  import {
-    AlertDialog,
-    AlertDialogBackdrop,
-    AlertDialogContent,
-    AlertDialogHeader,
-    AlertDialogCloseButton,
-    AlertDialogFooter,
-    AlertDialogBody,
-    Icon,
-    AlertCircleIcon,
-    Heading,
-    Button,
-    ButtonText
-  } from "@gluestack-ui/themed";
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogBody,
+  Icon,
+  AlertCircleIcon,
+  Heading,
+  Button,
+  ButtonText,
+} from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import { queryReportById, removeReportById } from "../../utils/Database/OfflineSQLiteDB";
-import Theme from "../../utils/Theme";
+import { removeReportById } from "../../utils/Database/OfflineSQLiteDB";
 import exportToCSV from "../../utils/Database/export";
+import Theme from "../../utils/Theme";
 import styles from "../ViewSavedReports/styles";
 
-export const ButtonContainer = ({reports}) => {
-    const navigation = useNavigation();
-    const [showAlertDialog, setShowAlertDialog] = React.useState(false)
+export const ButtonContainer = ({ reports }) => {
+  const navigation = useNavigation();
+  const [showConfirmDelete, setShowConfirmDelete] = React.useState(false);
+  const [showExportAlert, setShowExportAlert] = React.useState(false);
 
-    function compileReports(callback) {
-        const compiledReports = [];
-        for (var k in reports) {
-            if(reports[k.toString()]) { 
-                compiledReports.push(k);
-            }
-        }
-        callback(compiledReports);
+  function compileReports(callback) {
+    const compiledReports = [];
+    for (const k in reports) {
+      if (reports[k.toString()]) {
+        compiledReports.push(k);
+      }
     }
-    
+    callback(compiledReports);
+  }
 
-    const handleExport = () => {
-        console.log("reports being exported: " + JSON.stringify(reports, null, 2));
+  const handleExport = () => {
+    for (const k in reports) {
+      if (reports[k.toString()]) {
         compileReports(exportToCSV);
         navigation.navigate("MainScreen");
+        return;
+      }
     }
+    setShowExportAlert(true);
+  };
 
-    const handleDelete = () => {
-        setShowAlertDialog(false);
-        for (var k in reports) {
-            if(reports[k.toString()]) { 
-                console.log("Report being deleted: " + k);
-                removeReportById(k);
-            }
-            
-        }
-        navigation.navigate("MainScreen");
+  const handleDelete = () => {
+    setShowConfirmDelete(false);
+    for (const k in reports) {
+      if (reports[k.toString()]) {
+        console.log("Report being deleted: " + k);
+        removeReportById(k);
+      }
     }
+    navigation.navigate("MainScreen");
+  };
 
-    return (
-        <>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                onPress={handleExport}
-                style={styles.exportButton}
-            >
-                <Text style={styles.selectAllButtonText}>
-                    {"Export"}
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => setShowAlertDialog(true)}
-                style={styles.deleteButton}
-            >
-                <Text style={styles.selectAllButtonText}>
-                    {"Delete"}
-                </Text>
-            </TouchableOpacity>
-            </View>
-            <AlertDialog
-        isOpen={showAlertDialog}
+  return (
+    <>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleExport} style={styles.exportButton}>
+          <Text style={styles.selectAllButtonText}>Export</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setShowConfirmDelete(true)}
+          style={styles.deleteButton}
+        >
+          <Text style={styles.selectAllButtonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+      <AlertDialog
+        isOpen={showConfirmDelete}
         onClose={() => {
-          setShowAlertDialog(false)
+          setShowConfirmDelete(false);
         }}
       >
         <AlertDialogBackdrop />
         <AlertDialogContent>
           <AlertDialogHeader>
-              <Heading size="lg">WARNING</Heading>
-              <Icon
-                as={AlertCircleIcon}
-                color={Theme.COLORS.WARNING}
-              />
+            <Heading size="lg">WARNING</Heading>
+            <Icon as={AlertCircleIcon} color={Theme.COLORS.WARNING} />
           </AlertDialogHeader>
           <AlertDialogBody>
             <Text size={Theme.TYPOGRAPHY.FONT_SIZE.LARGE}>
-              Once reports are deleted, they can no longer be edited or exported in the app. Make
-              sure all reports are properly exported and transferred from your device before deleting.
+              Once reports are deleted, they can no longer be edited or exported
+              in the app. Make sure all reports are properly exported and
+              transferred from your device before deleting.
             </Text>
           </AlertDialogBody>
           <AlertDialogFooter>
@@ -117,7 +107,7 @@ export const ButtonContainer = ({reports}) => {
               action="secondary"
               mr="$3"
               onPress={() => {
-                setShowAlertDialog(false)
+                setShowConfirmDelete(false);
               }}
             >
               <ButtonText>Go back</ButtonText>
@@ -125,7 +115,37 @@ export const ButtonContainer = ({reports}) => {
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-        </>
-
-    );
+      <AlertDialog
+        isOpen={showExportAlert}
+        onClose={() => {
+          setShowExportAlert(false);
+        }}
+      >
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading size="lg" />
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            <Text size={Theme.TYPOGRAPHY.FONT_SIZE.SMALL} textAlign="center">
+              Please select at least one report to export.
+            </Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button
+              variant="outline"
+              size="sm"
+              action="secondary"
+              mr="$3"
+              onPress={() => {
+                setShowExportAlert(false);
+              }}
+            >
+              <ButtonText>Go back</ButtonText>
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 };

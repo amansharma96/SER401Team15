db8eb40eb4ffbb4e5300c4d165285952e079334d
Sheldon Pang <SheldonLinkedin@yahoo.com>
2024-03-29 20:58:59 -0700

    setup Drawer navigation

--- EmergencyDataCollectionApp/App.js
+++ EmergencyDataCollectionApp/App.js
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler"; // Must be at the top of the file
+
 import { config } from "@gluestack-ui/config";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { NavigationContainer } from "@react-navigation/native";

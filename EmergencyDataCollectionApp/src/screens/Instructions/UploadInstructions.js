ddbc945209edf5f2e8dac1e75795df0cd2fe00f9
David Hanley <dchanley@asu.edu>
2024-04-02 20:38:10 -0700

    Fixed Instructions page. Need help with images

--- EmergencyDataCollectionApp/src/screens/Instructions/UploadInstructions.js
+++ EmergencyDataCollectionApp/src/screens/Instructions/UploadInstructions.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from "./styles";
-import Images from "../../utils/Images";
 
 const UpLoadInstructions = () => {
   return (
@@ -12,32 +11,31 @@ const UpLoadInstructions = () => {
         1. Connect a USB storage device to your Android device.
       </Text>
       <Text style={styles.instruction}>
-        2. On your Android device, open Files by Google Files by Google
-        <Image source={Images.downArrow}/>
-        .
+        2. On your Android device, open Files by Google Files by Google.
       </Text>
       <Text style={styles.instruction}>
         3. Under 'Storage devices', tap your USB storage device.
       </Text>
       <Text style={styles.instruction}>4. Find the file you want to copy.</Text>
       <Text style={styles.instruction}>
-        5. Next to a file you want to copy, tap the{" "}
-        <Image source={Images.downArrow} />. If you can't find the{" "}
-        <Image source={Images.downArrow} /> next to the files, tap List view
-        List.
+        5. Next to a file you want to copy, tap the Down arrow. If you can't
+        find the Down arrow next to the files, tap List view List.
       </Text>
       <Text style={styles.instruction}>
-        - To copy one file: Tap the <Image source={Images.downArrow} /> and then
-        Copy to.
+        {" "}
+        - To copy one file: Tap the Down arrow and then Copy to.
       </Text>
-      <Text style={styles.instruction}>- To copy multiple files:</Text>
+      <Text style={styles.instruction}> - To copy multiple files:</Text>
       <Text style={styles.instruction}>
-        a. Tap the <Image source={Images.downArrow} /> and then Select.
+        {" "}
+        a. Tap the Down arrow and then Select.
       </Text>
       <Text style={styles.instruction}>
+        {" "}
         b. Next to each file you want to copy, check the circle.
       </Text>
       <Text style={styles.instruction}>
+        {" "}
         c. At the top, tap More More and then Copy to.
       </Text>
       <Text style={styles.instruction}>

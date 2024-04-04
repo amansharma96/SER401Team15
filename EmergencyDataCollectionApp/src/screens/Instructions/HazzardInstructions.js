d0864615c980506f2770dd3575390361ce16f00b
David Hanley <dchanley@asu.edu>
2024-04-04 16:09:20 -0700

    Updated instruction pages

--- EmergencyDataCollectionApp/src/screens/Instructions/HazzardInstructions.js
+++ EmergencyDataCollectionApp/src/screens/Instructions/HazzardInstructions.js
@@ -1,26 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 
 import styles from "./styles";
 
 const HazzardInstructions = () => {
-  const instructions = [
-    "Let the GPS get as accurate reading as possible",
-    "Make notes of everything",
-    "The report is guided, fill out the page before moving to the next one",
-    "Document everything in the notes section you feel is important",
-    "Take pictures of anything important and add them to the report",
-  ];
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hazzard Report Instructions</Text>
-      <Text style={styles.placehodler}>*Place Holder*</Text>
-      {instructions.map((instruction, index) => (
-        <Text key={index} style={styles.instruction}>
-          • {instruction}
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.placehodler}>Starting a HAZARD report</Text>
+        <Text style={styles.text}>
+          Select the New HAZARD report button. There are 3 sections you will go
+          through. REPORT, DETAILS, and REVIEW
         </Text>
-      ))}
-    </View>
+        <Text style={styles.placehodler}>The Report section:</Text>
+        <Text style={styles.text}>
+          First an automatic date and timestamp as you start the report. You can
+          manually edit these if needed. We will automatically capture GPS
+          location. For most accurate results please stand a safe distance from
+          the hazard you are documenting. If the GPS location accuracy is low
+          the data will appear red in color. Try again. You may need to move.
+          Yellow indicates a bit more accuracy. If the data shows green you are
+          good to accept the data.
+        </Text>
+        <Text style={styles.placehodler}>The Details Section:</Text>
+        <Text style={styles.text}>
+          This section lets you select the hazard you want to report. And gives
+          you the opportunity to photograph the hazard and add any description
+          you feel is required.
+        </Text>
+        <Text style={styles.placehodler}>The Review Section:</Text>
+        <Text style={styles.text}>
+          You will have an opportunity to review and edit this report before
+          saving this file.
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 
--- EmergencyDataCollectionApp/src/screens/Instructions/HomeInstructions.js
+++ EmergencyDataCollectionApp/src/screens/Instructions/HomeInstructions.js
@@ -6,27 +6,39 @@ import styles from "./styles";
 const HomeInstructions = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>App Instructions</Text>
       <Text style={styles.placehodler}>What this App is for:</Text>
       <Text style={styles.text}>
-        The Emergency Ready app is used by Map your Neighborhood groups, CERT
-        search and rescue teams, and public sector workers to gather critical
-        information for utilization by Emergency Operations Centers immediately
-        after a major disaster.
+        The NeighborCheck app is used by READY NEIGHBOR groups, CERT search and
+        rescue teams, and public sector workers to gather critical information
+        for utilization by Emergency Operations Centers immediately after a
+        major disaster.
       </Text>
       <Text style={styles.placehodler}>How the App is organized:</Text>
       <Text style={styles.text}>
-        The App is divided into 3 sections that are specifically tailored to MYN
-        activities, CERT activities, and hazards that public sector workers may
-        come across and wish to report. The sections of the app for MYN and CERT
-        activity are modeled after the FEMA rapid needs assessment Form and are
-        tailored to match their activity. The section used by public sector
-        workers allows them to locate, describe, and photograph locations that
-        may be hazards to the public and impede rescue efforts. Public sector
-        workers may include, PUD workers, City and county road crews, Police and
-        Fire personnel. For MYN and CERT users of this App, the data collection
-        sequence mirrors the natural sequence of search and rescue activity as
-        teams go house to house.
+        The App is divided into 3 sections that are specifically tailored to
+        READY NEIGHBOR activities, CERT activities, and hazards that public
+        sector workers and other may come across and wish to report. The
+        sections of the app for READY NEIGHBOR and CERT activity are modeled
+        after the FEMA rapid needs assessment Form and are tailored to match
+        their activity sequence. The section used by public sector workers
+        allows them to locate, describe, and photograph locations that may be
+        hazards to the public and impede rescue efforts. Public sector workers
+        may include, PUD workers, City and county road crews, Police and Fire
+        personnel. For READY NEIGHBOR and CERT users of this App, the data
+        collection sequence mirrors the natural sequence of search and rescue
+        activity as teams go house to house.
+      </Text>
+      <Text style={styles.placehodler}>
+        How to use the NeighborCheck App- Tips for saving time:
+      </Text>
+      <Text style={styles.text}>
+        The SET USER DEFAULTS button To help make the data entry faster you can
+        pre-load certain common data field answers like City , State and ZIP
+        code. Press the “set User Defaults” button and fill out only the fields
+        that pertain to your activity. READY NEIGHBOR people only fill out READY
+        NEIGHBOR Group Name fields CERT people only fill out the Cert group
+        number and Cert Squad Name fields ALL users fill out the CITY, ZIP, and
+        STATE fields
       </Text>
     </View>
   );
